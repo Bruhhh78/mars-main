a3d1f833a39d60461f4288cb55dcea8f07aa52f9
Anmol Srivastava <anmolsrivastava678@gmail.com>
2024-11-04 22:54:18 +0530

    Git Brands Initializing

---
+++ src/components/Brands/Brands.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+const Brands = () => {
+  return (
+    <section
+      style={{ padding: "40px", backgroundColor: "white", textAlign: "center" }}
+    >
+      <div style={{ marginBottom: "20px" }}>
+        <h2 style={{ fontSize: "2em", color: "#333" }}>Brands We Work With</h2>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "40px",
+          overflowX: "auto",
+          whiteSpace: "nowrap",
+        }}
+      >
+        <figure
+          style={{ width: "220px", height: "auto", display: "inline-block" }}
+        >
+          <img
+            src="https://cdn.dribbble.com/users/432807/screenshots/14567438/rh.jpg"
+            alt="Phoenix"
+            style={{ maxWidth: "100%", height: "auto", objectFit: "contain" }}
+          />
+        </figure>
+        <figure
+          style={{ width: "220px", height: "auto", display: "inline-block" }}
+        >
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8HuI1EL43YRf2TX1R3a3TSgGuFonHywAvvA&s"
+            alt="Godrej Interio"
+            style={{ maxWidth: "100%", height: "auto", objectFit: "contain" }}
+          />
+        </figure>
+        <figure
+          style={{ width: "120px", height: "auto", display: "inline-block" }}
+        >
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLp2aQDxpKoef3IOhaMu8iAwL2piqkSi9V9rPQEsOzgzi57wBPjGUzBXs6z8J09Iug1Ic&usqp=CAU"
+            alt="Philips"
+            style={{ maxWidth: "100%", height: "auto", objectFit: "contain" }}
+          />
+        </figure>
+        <figure
+          style={{ width: "250px", height: "auto", display: "inline-block" }}
+        >
+          <img
+            src="https://i0.wp.com/trendystockcharts.com/wp-content/uploads/2017/08/MASI-Logo-866W-H.jpg?fit=866%2C541&ssl=1"
+            alt="Masimo"
+            style={{ maxWidth: "100%", height: "auto", objectFit: "contain" }}
+          />
+        </figure>
+      </div>
+    </section>
+  );
+};
+
+export default Brands;
--- src/pages/WebPage.jsx
+++ src/pages/WebPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../components/Banner/Banner.jsx";
+import Brands from "../components/Brands/Brands.jsx";
 
 const WebPage = () => {
   return (
@@ -7,6 +8,7 @@ const WebPage = () => {
       <div  style={{ background: "#458ff6",minHeight:"65vh",color:"white", overflow: "hidden" }}>
         <Banner/>
       </div>
+      <Brands/>
     </div>
   );
 };
