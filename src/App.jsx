0cb020e251fb89dd6c6c842be7ec1eeff41df841
Anmol Srivastava <anmolsrivastava678@gmail.com>
2024-11-05 23:01:32 +0530

    Git Mars Initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout.jsx";
 import WebPage from "./pages/WebPage.jsx";
@@ -10,34 +9,22 @@ import Contact from "./components/Contact/Contact.jsx";
 import TieupBrands from "./components/TieupBrands/TieupBrands.jsx";
 import Appointment from "./components/Appointment/Appointment.jsx";
 import Maintenance from "./components/Maintenance/Maintenance.jsx";
+import CustomMouse from "./components/CustomMouse/CustomMouse.jsx"; // Import the CustomMouse component
 
 const App = () => {
   return (
     <BrowserRouter>
+      <CustomMouse /> {/* Add the CustomMouse component here */}
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<WebPage />} />
-          <Route path="/products">
-            <Route index element={<Products />} />
-          </Route>
-          <Route path="/services">
-            <Route index element={<Services />} />
-          </Route>
-          <Route path="/about">
-            <Route index element={<About />} />
-          </Route>
-          <Route path="/contact">
-            <Route index element={<Contact />} />
-          </Route>
-          <Route path="/brands">
-            <Route index element={<TieupBrands />} />
-          </Route>
-          <Route path="/appointment">
-            <Route index element={<Appointment />} />
-          </Route>
-          <Route path="/maintenance">
-            <Route index element={<Maintenance />} />
-          </Route>
+          <Route path="/products" element={<Products />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/brands" element={<TieupBrands />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/maintenance" element={<Maintenance />} />
         </Route>
       </Routes>
     </BrowserRouter>
