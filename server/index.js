81089377642dcec7d9575fbda41bb28d37920cd8
Anmol Srivastava <anmolsrivastava678@gmail.com>
2024-11-06 21:52:58 +0530

    Git Database Connected With Prisma

---
+++ server/index.js
@@ -0,0 +1,27 @@
+// Import necessary modules
+import express from "express"; // Express framework for building the server
+import dotenv from "dotenv"; // dotenv module to load environment variables from a .env file
+import cookieParser from "cookie-parser"; // Middleware to parse cookies
+import cors from "cors"; // Middleware to handle Cross-Origin Resource Sharing (CORS)
+
+dotenv.config(); // Load environment variables
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Middleware setup
+app.use(express.json()); // Parse JSON bodies
+app.use(cookieParser()); // Parse cookies
+
+// CORS configuration
+app.use(cors());
+
+app.get("/", (req, res) => {
+  console.log("Backend Working!");
+  res.send("Hello World!");
+});
+
+// Start the server and listen on the defined port
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/`);
+});
