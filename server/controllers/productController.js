ad912f199ba4be08129433a8298fc6472127b1d6
Anmol Srivastava <anmolsrivastava678@gmail.com>
2024-11-07 13:24:13 +0530

    Git Initialized Product Creation, User Creation Working Properly and ProductData Uploaded to Database

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -19,6 +19,16 @@ export const createProduct = asyncHandler(async (req, res) => {
             return res.status(404).send({ message: 'User not found' });
         }
 
+        // Check if a product with the same name already exists for the same user
+        const existingProduct = await prisma.productData.findFirst({
+            where: { name, userEmail },
+        });
+
+        if (existingProduct) {
+            return res.status(400).send({ message: 'Product already exists' });
+        }
+
+        // Create the new product if it doesn't already exist
         const product = await prisma.productData.create({
             data: {
                 name,
@@ -32,12 +42,6 @@ export const createProduct = asyncHandler(async (req, res) => {
         res.status(201).send({ message: 'Product created successfully', product });
     } catch (error) {
         console.error("Error during product creation:", error);  // Log the error
-        if (error.code === 'P2002') {
-            res.status(400).send({
-                message: "A product with these specs already exists",
-            });
-        } else {
-            res.status(500).send({ message: 'Internal Server Error', error: error.message });
-        }
+        res.status(500).send({ message: 'Internal Server Error', error: error.message });
     }
 });
