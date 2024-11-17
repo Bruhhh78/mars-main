569f81c42e0ebfef658b5e0e5f29056d26b3fd47
Anmol Srivastava <anmolsrivastava678@gmail.com>
2024-11-17 14:14:42 +0530

    git initialised

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: import.meta.env.VITE_AUTH0_REDIRECT_URI,
+        redirect_uri: "https://mars-healthcare.vercel.app/",
       }}
       audience={import.meta.env.VITE_AUTH0_AUDIENCE}
       scope="openid profile email"
