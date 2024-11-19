8365bf7725d7826ae5c2657d7fb9241406ec72bf
Anmol Srivastava <98909007+Bruhhh78@users.noreply.github.com>
2024-11-19 11:26:59 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import { Auth0Provider } from "@auth0/auth0-react";
+// import App from "./App.jsx";
+// import { Auth0Provider } from "@auth0/auth0-react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Auth0Provider
+{/*     <Auth0Provider
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
@@ -14,8 +14,8 @@ createRoot(document.getElementById("root")).render(
       }}
       audience={import.meta.env.VITE_AUTH0_AUDIENCE}
       scope="openid profile email"
-    >
+    > */}
       <App />
-    </Auth0Provider>
+{/*     </Auth0Provider> */}
   </StrictMode>
 );
