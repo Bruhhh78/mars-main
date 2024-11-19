f8df587d68cf9d0a23bb7e99ba23a13f5abc78da
Anmol Srivastava <98909007+Bruhhh78@users.noreply.github.com>
2024-11-19 11:47:37 +0530

    Update common.js

--- client/src/utils/common.js
+++ client/src/utils/common.js
@@ -1,12 +1,12 @@
-export const updateFavourites = (id, favourites) => {
-    if (favourites.includes(id)) {
-      return favourites.filter((prdId) => prdId !== id);
-    } else {
-      return [...favourites, id];
-    }
-  };
+// export const updateFavourites = (id, favourites) => {
+//     if (favourites.includes(id)) {
+//       return favourites.filter((prdId) => prdId !== id);
+//     } else {
+//       return [...favourites, id];
+//     }
+//   };
   
-  export const checkFavourites = (id, favourites) => {
-    return favourites?.includes(id) ? "#fa3e5f" : "white";
-  };
+//   export const checkFavourites = (id, favourites) => {
+//     return favourites?.includes(id) ? "#fa3e5f" : "white";
+//   };
   
