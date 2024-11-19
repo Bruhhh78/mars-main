f1fcc54c93442ac12d08a5f73030b5e7cde31c51
Anmol Srivastava <98909007+Bruhhh78@users.noreply.github.com>
2024-11-19 11:53:07 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,21 +1,21 @@
-import express from "express";
-import {
-  bookVisit,
-  cancelBooking,
-  createUser,
-  getAllBookings,
-  getAllFavourites,
-  toFav,
-} from "../controllers/userController.js";
-import jwtCheck from "../config/auth0Config.js";
+// import express from "express";
+// import {
+//   bookVisit,
+//   cancelBooking,
+//   createUser,
+//   getAllBookings,
+//   getAllFavourites,
+//   toFav,
+// } from "../controllers/userController.js";
+// import jwtCheck from "../config/auth0Config.js";
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post("/register", jwtCheck, createUser);
-router.post("/bookvisit/:id", jwtCheck, bookVisit);
-router.get("/allBookings",jwtCheck, getAllBookings);
-router.post("/cancelBooking/:id", jwtCheck, cancelBooking);
-router.post("/toFav/:pid", jwtCheck, toFav);
-router.get("/allFavs", getAllFavourites);
+// router.post("/register", jwtCheck, createUser);
+// router.post("/bookvisit/:id", jwtCheck, bookVisit);
+// router.get("/allBookings",jwtCheck, getAllBookings);
+// router.post("/cancelBooking/:id", jwtCheck, cancelBooking);
+// router.post("/toFav/:pid", jwtCheck, toFav);
+// router.get("/allFavs", getAllFavourites);
 
-export { router as userRoute };
+// export { router as userRoute };
