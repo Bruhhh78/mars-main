adffe73809cb1a9d7efd54b3baa9fe007a8a4bb1
Anmol Srivastava <98909007+Bruhhh78@users.noreply.github.com>
2024-11-20 11:53:12 +0530

    Update ChatButton.jsx

--- client/src/components/Buttons/ChatButton.jsx
+++ client/src/components/Buttons/ChatButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BsFillChatDotsFill } from "react-icons/bs";
 
 const ChatButton = () => {
-  const phoneNumber = '8808055633'; 
+  const phoneNumber = '918808055633'; 
   const message = 'Hello, I would like to enquire about your product...';
 
   const handleWhatsAppClick = () => {
